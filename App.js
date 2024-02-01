38575bf424a39a3a8b1f13c415f25e265f3e0e15
Fellipe <fellipealbert3@gmail.com>
2024-02-01 16:28:50 -0300

    using textInput and button

--- App.js
+++ App.js
@@ -1,47 +1,47 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [name, setName]= useState("")
+  const [wellcome, setWellcome]= useState("")
+
+
+  const handleOnChangeText = (value)=>{
+    if(value ==''){
+      setWellcome("")
+    }
+     setName(value)
+  }
+
+  const handleOnPress = ()=>{
+    if(name==''){
+      alert("Digite seu nome!")
+    }
+    setWellcome("Bem vindo")
+ }
   return (
     <View style={styles.container}>
-      <View style={{flex:1, backgroundColor:'red'}}>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-        <Text>Olá que tal </Text>
-
-        </View>
-      <View style={{flex:1, backgroundColor:'yellow'}}>
-        <Text>Olá que tal </Text></View>
-      <View style={{flex:1, backgroundColor:'green'}}>
-         <Text>Olá que tal </Text>
-      </View>
-
-      {/* <Button title="Entrar" OnPress={()=>{return}} /> */}
-  
+      <TextInput value={name} style={styles.input} placeholder='Digite seu nome' underlineColorAndroid='transparent' onChangeText={handleOnChangeText}/>
+      <Button title="Entrar"onPress={handleOnPress}/>
+     {wellcome && name && <Text style={{textAlign:"center"}}> Bem vindo: {name}</Text>}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    // width:340,
-    // height:100,
     flex: 1,
-    marginTop: 30,
-    backgroundColor: 'gray',
-    // alignItems: 'center',
-     justifyContent: 'center',
+    marginTop:45,
   },
+  input:{
+    
+    height:45,
+    borderWidth:1,
+    borderColor:"#222",
+    margin:10,
+    fontSize:20,
+    padding:10,
+  }
 });
